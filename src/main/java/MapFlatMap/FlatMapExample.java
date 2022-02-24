183d398162fe8075cc6316af9850dba1cdb2be8a
deepa <deepakarra@gmail.com>
2022-02-24 19:52:52 +0100

    Adding example for flatmap

---
+++ src/main/java/MapFlatMap/FlatMapExample.java
@@ -0,0 +1,30 @@
+package MapFlatMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+
+public class FlatMapExample {
+
+	public static void main (String[] args) {
+		List<String>emp1Dep = new ArrayList();
+		emp1Dep.add("dep1");
+		emp1Dep.add("dep2");
+		List<String>emp2Dep = new ArrayList<>();
+		emp2Dep.add("dep3");
+		emp2Dep.add("dep2");
+		
+		List<Employee> empList= Arrays.asList(new Employee(1,"emp1",emp1Dep),new Employee(1,"emp2",emp2Dep));
+	
+		System.out.println(empList.stream()
+        .flatMap(x->x.getDepartment().stream())
+        .collect(Collectors.toSet()));   
+		}
+}
+
+
