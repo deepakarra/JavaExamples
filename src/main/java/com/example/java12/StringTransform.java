68530566e64f5a62a6e23c8d665f674673468523
deepa <deepakarra@gmail.com>
2022-09-20 21:20:56 +0200

    Adding String Indentation and transformation examples

--- src/main/java/com/example/java12/StringTransform.java
+++ src/main/java/com/example/java12/StringTransform.java
@@ -5,7 +5,7 @@ public class StringTransform {
 	public static void main(String[] args) {
 		String str = "Hello World";
 		System.out.println("before transformation "+ str);
-		String transformedStr = str.transform(x->x .toUpperCase()+str);
+		String transformedStr = str.transform(x->x .toUpperCase()+ " " + str);
 		System.out.println("transformed string: "+ transformedStr);
 	}
 
